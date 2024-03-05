d7945d1c12763a64859e4ec77966896a575133ec
Luccas <luccas_henrique123@hotmail.com>
2024-03-05 14:22:50 -0300

    atualizacao media

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Navbar } from './components/navbar'
 import { Home } from './components/home'
 import { About } from './components/about'
 import { Project } from './components/projects'
+import { Footer } from './components/footer'
 
 export function App() {
 
   return (
     <>
       <Navbar />
-      <Home/>
-      <About/>
-      <Project/>
+      <Home />
+      <About />
+      <Project />
+      <Footer />
     </>
   )
 }
